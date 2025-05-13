446ecb2afd54217fdf13a9d028b08fbd7aaa1dd0
kristen <kristen@teialabs.com>
2025-05-13 17:08:39 -0300

    proxy working

--- stream-testing/server.py
+++ stream-testing/server.py
@@ -1,24 +1,30 @@
 import asyncio
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(root_path="/agent1")
 
-@app.post('/chat')
-async def chat():
+
+class InputBody(BaseModel):
+    input: str
+
+
+@app.post("/chat")
+async def chat(input_body: InputBody):
     return StreamingResponse(
-        event_stream(),
-        media_type='text/event-stream',
+        event_stream(input_body),
+        media_type="text/event-stream",
+        headers={"X-Accel-Buffering": "no"},
     )
 
-async def event_stream():
+
+async def event_stream(body: InputBody):
     for i in range(5):
-        yield f"data: Hello, world! {i}\n\n"
+        yield f"data: Hello, world! {i} - {body.input}\n\n"
         await asyncio.sleep(3)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
-
